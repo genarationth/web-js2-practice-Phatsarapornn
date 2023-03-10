56f359c512f40a7f4fa0726ded012fc93a6468c3
Phatsaraporn <phatsaraporn.j@gmail.com>
2023-03-11 01:52:31 +0700

    answer part2

---
+++ script_part2-new-contender.js
@@ -0,0 +1,15 @@
+// Challenge 1: Create an Age Calculator
+let birthYear = 1995;
+let futureYear = 2026;
+let futureAge = futureYear - birthYear;
+console.log(`If you were born in ${birthYear}, then in ${futureYear} you'll be ${futureAge-1} or ${futureAge} (depending on the month in ${futureYear}).`);
+
+
+
+// Challenge 2: Create an information card
+let yourName ="Paifon";
+let yourLocation = "Thailand";
+let yourHobby ="sleeping";
+console.log(`\"Hi, my name is ${yourName}. I live in ${yourLocation} and enjoy ${yourHobby}.\"`);
+
+
